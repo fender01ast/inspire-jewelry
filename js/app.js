296ab949c5c4323312b9d0ea210f07af89a5e54d
fender01ast <rus.k1996@gmail.com>
2021-06-09 00:52:45 +0600

    add progress bar, toTopBtn and yandex metrika script

--- js/app.js
+++ js/app.js
@@ -5,4 +5,26 @@ document.addEventListener("DOMContentLoaded", function(){
     document.querySelector('.header__logo-img').addEventListener('click', function(){
         window.open('https://www.instagram.com/inspire__jewelry/');
     });
+
+    let $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+        return false;
+    });
+
+    let button = $('.toTopBtn');
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 50) {
+            button.fadeIn();
+          } else {
+            button.fadeOut();
+          }
+    });
+
+    $(window).scroll(function() {
+        let ratio = $(document).scrollTop() / (($(document).height() - $(window).height()) / 100);
+        $("#progress").width(ratio + "%");
+    });
 });
