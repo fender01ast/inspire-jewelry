d037076d216ea9315f0db12253da6aae423fe729
fender01ast <rus.k1996@gmail.com>
2021-06-17 17:11:44 +0600

    add animations, 2 additional pages, 2gis map, lightbox, splide lib for sliders

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
+    new WOW().init();
+
     let currentYear = new Date().getFullYear();
     document.getElementById('year-now').innerHTML = currentYear;
 
     document.querySelector('.header__logo-img').addEventListener('click', function(){
-        window.open('https://www.instagram.com/inspire__jewelry/');
+        window.open('index.html');
     });
 
     let $page = $('html, body');
@@ -15,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 
     let button = $('.toTopBtn');
+    button.fadeOut();
     $(window).on('scroll', () => {
         if ($(this).scrollTop() >= 50) {
             button.fadeIn();
@@ -27,4 +30,86 @@ document.addEventListener("DOMContentLoaded", function(){
         let ratio = $(document).scrollTop() / (($(document).height() - $(window).height()) / 100);
         $("#progress").width(ratio + "%");
     });
+    
+    if (document.location.pathname.indexOf('/mega.html') >= 0) {    // если страница === мега
+        /* <slider mega> */
+        new Splide( '.splide', {
+            type        : 'loop',
+            perPage     : 3,
+            focus       : 'center',
+            width       : '100%',
+            height      : '320px',
+            //trimSpace   : true,
+            pagination  : true,
+            arrows      : true,
+            autoplay    : true,
+            interval    : '2500',
+            drag        : true,
+            lazyLoad    : true,
+            gap         : '10px',
+            breakpoints : {
+                767: {
+                    perPage  : 1,
+                    //gap      : '10px',
+                    height   : '225px',
+                },
+                991: {
+                    perPage  : 2,
+                }
+            }
+        } ).mount();
+        /* </slider mega> */
+
+        var map;
+        DG.then(function () {
+            map = DG.map('map', {
+                center: [51.089758, 71.407374],
+                zoom: 16
+            });
+
+            DG.marker([51.089581, 71.406209]).addTo(map).bindPopup('Inspire Jewelry Mega Silkway');
+            DG.marker([51.164572, 71.44242]).addTo(map).bindPopup('Inspire Jewelry Alma-Ata');
+        });
+    }
+
+    if (document.location.pathname.indexOf('/alma-ata.html') >= 0) {    // если страница === алма-ата
+        /* <slider alma-ata> */
+        new Splide( '.splide', {
+            type        : 'loop',
+            perPage     : 3,
+            focus       : 'center',
+            width       : '100%',
+            height      : '320px',
+            //trimSpace   : true,
+            pagination  : true,
+            arrows      : true,
+            autoplay    : true,
+            interval    : '2500',
+            drag        : true,
+            lazyLoad    : true,
+            gap         : '10px',
+            breakpoints : {
+                767: {
+                    perPage  : 1,
+                    //gap      : '10px',
+                    height   : '225px',
+                },
+                991: {
+                    perPage  : 2,
+                }
+            }
+        } ).mount();
+        /* </slider alma-ata> */
+
+        var map;
+        DG.then(function () {
+            map = DG.map('map', {
+                center: [51.164246, 71.441412],
+                zoom: 16
+            });
+
+            DG.marker([51.089581, 71.406209]).addTo(map).bindPopup('Inspire Jewelry Mega Silkway');
+            DG.marker([51.164572, 71.44242]).addTo(map).bindPopup('Inspire Jewelry Alma-Ata');
+        });
+    }
 });
